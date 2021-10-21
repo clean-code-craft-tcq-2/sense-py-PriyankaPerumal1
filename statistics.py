6eaa440ebd39b2ce48fdd8041c6f318d01260eb5
PriyankaPerumal1 <90901203+PriyankaPerumal1@users.noreply.github.com>
2021-10-21 21:08:55 +0530

    changes

--- statistics.py
+++ statistics.py
@@ -1,3 +1,8 @@
 
 def calculateStats(numbers):
-  return None
+  computeStats = []
+  if(len(numbers)!=0):
+    computeStats["min"] = min(numbers)
+    computeStats["max"] = max(numbers)
+    computeStats["avg"] = sum(numbers)/len(numbers)
+  return computeStats
